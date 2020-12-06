560555e8504c7154159fa53da6c516d99ef5e7db
yeroc <yeroc@geekden.org>
2020-12-05 20:44:25 -0700

    Minor cleanup.

--- framework/src/main/java/org/geekden/advent/Driver.java
+++ framework/src/main/java/org/geekden/advent/Driver.java
@@ -11,38 +11,38 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 class Driver {
-  private final Set<Solver> days;
+  private final Set<Solver> solutions;
   private final Path inputBasePath;
 
-  Driver(Set<Solver> days, Path inputBasePath) throws Exception {
-    this.days = days;
+  Driver(Set<Solver> solutions, Path inputBasePath) throws Exception {
+    this.solutions = solutions;
     this.inputBasePath = inputBasePath;
   }
 
   void execute() throws Exception {
-    for (Solver day : days) {
-      Path inputFile = inputBasePath.resolve(format("%d/%02d/input.txt", day.year(), day.day()));
+    for (Solver solution : solutions) {
+      Path inputFile = inputBasePath.resolve(format("%d/%02d/input.txt", solution.year(), solution.day()));
 
       if (Files.notExists(inputFile)) {
-        System.err.printf("Input file %s not found for %d, day %d.\n", inputFile, day.year(), day.day());
+        System.err.printf("Input file %s not found for %d, day %d.\n", inputFile, solution.year(), solution.day());
         continue;
       }
 
-      String partOneSolution = solve(day::solvePartOne, inputFile);
-      printSolution(day, partOneSolution, "I");
+      String partOneSolution = solve(solution::solvePartOne, inputFile);
+      printSolution(solution, partOneSolution, "I");
 
-      String partTwoSolution = solve(day::solvePartTwo, inputFile);
-      printSolution(day, partTwoSolution, "II");
+      String partTwoSolution = solve(solution::solvePartTwo, inputFile);
+      printSolution(solution, partTwoSolution, "II");
     }
   }
 
-  private String solve(Function<Stream<String>, String> fn, Path inputFile) throws IOException {
+  private static String solve(Function<Stream<String>, String> fn, Path inputFile) throws IOException {
     try (Stream<String> input = Files.lines(inputFile, StandardCharsets.UTF_8)) {
       return fn.apply(input);
     }
   }
 
-  private void printSolution(Solver day, String solution, String part) {
+  private static void printSolution(Solver day, String solution, String part) {
     System.out.printf("Solution for %d, day %d, part %-2s: [%25s]\n",
         day.year(), day.day(), part, solution);
   }
