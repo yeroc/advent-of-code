6e75d4c05ca6359d00e2aa2b7474b5043a26a8f8
yeroc <yeroc@geekden.org>
2020-12-08 00:10:26 -0700

    Add solution for 2020-08.

---
+++ solutions/src/main/java/org/geekden/advent/solution/y2020/d08/Solution.java
@@ -0,0 +1,144 @@
+package org.geekden.advent.solution.y2020.d08;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.geekden.advent.Solver;
+
+import com.google.common.base.MoreObjects;
+
+public class Solution extends Solver {
+
+  public Solution() {
+    super(2020, 8);
+  }
+
+  @Override
+  public String solvePartOne(Stream<String> input) {
+    Computer computer = new Computer(parseProgram(input));
+    try {
+      computer.run();
+    } catch (IllegalStateException e) {
+      // crash is expected!
+    }
+    return String.valueOf(computer.accumulator);
+  }
+
+  @Override
+  public String solvePartTwo(Stream<String> input) {
+    List<Instruction> program = parseProgram(input);
+    Computer computer = new Computer(new ArrayList<>(program));
+    int potentialCorruptedOffset = 0;
+    while (potentialCorruptedOffset <= computer.program.size()) {
+      for (int i = potentialCorruptedOffset; i < computer.program.size(); i++) {
+        Instruction instr = computer.program.get(i);
+        if (instr.op == Operation.ACC) continue;
+        potentialCorruptedOffset = i + 1;
+        computer.program.set(
+            i,
+            new Instruction(
+                instr.offset,
+                instr.op == Operation.JMP ? Operation.NOP : Operation.JMP,
+                instr.parameter));
+        break;
+      }
+
+      try {
+        computer.run();
+        break;
+      } catch (IllegalStateException e) {
+        // still crashing, reset state & try again...
+        computer = new Computer(new ArrayList<>(program));
+        continue;
+      }
+    }
+
+    return String.valueOf(computer.accumulator);
+  }
+
+  List<Instruction> parseProgram(Stream<String> input) {
+    AtomicInteger offsetCounter = new AtomicInteger();
+    return input.map(l -> {
+        String[] opAndParameter = l.split(" ");
+        return new Instruction(offsetCounter.getAndIncrement(), Operation.valueOf(opAndParameter[0].toUpperCase()), Long.valueOf(opAndParameter[1]));
+      })
+      .collect(Collectors.toList());
+  }
+
+  static class Computer {
+    long accumulator = 0l;
+    int instructionPointer = -1;
+    Instruction lastInstr;
+    List<Instruction> program;
+
+    Computer(List<Instruction> program) {
+      this.program = program;
+      for (Instruction instruction : program) {
+        instruction.executed = false;
+      }
+    }
+
+    void run() {
+      while (advance()) {
+//        debug();
+      }
+    }
+
+    boolean advance() {
+      instructionPointer++;
+      if (instructionPointer == program.size()) {
+        return false; // normal exit
+      }
+
+      Instruction instr = program.get(instructionPointer);
+
+      if (instr.executed) {
+        throw new IllegalStateException("Infinite loop detected!");
+      }
+
+      switch (instr.op) {
+      case ACC:
+        accumulator += instr.parameter;
+        break;
+      case JMP:
+        instructionPointer += instr.parameter - 1;
+      default:
+        break;
+      }
+      instr.executed = true;
+      lastInstr = instr;
+      return true;
+    }
+
+    void debug() {
+      System.out.printf("%s: acc: %d, istr: %d\n", lastInstr, accumulator, instructionPointer);
+    }
+  }
+
+  static enum Operation {
+    ACC, JMP, NOP
+  }
+
+  static class Instruction {
+    public final int offset;
+    public final Operation op;
+    public final long parameter;
+    public boolean executed = false;
+
+    public Instruction(int offset, Operation op, long parameter) {
+      this.offset = offset;
+      this.op = op;
+      this.parameter = parameter;
+    }
+
+    @Override
+    public String toString() {
+      return MoreObjects.toStringHelper(Instruction.class)
+          .add("op", op).add("parameter", parameter).add("executed", executed)
+          .toString();
+    }
+  }
+}
